59ca5aa8f05cf44ea24a60e25ff7f69e18a9b279
xfwdrev <74693315+xfwdrev@users.noreply.github.com>
2026-08-05 08:50:22 +0800

    build/build.sh: don't hardcode custom defconfig
    Signed-off-by: xfwdrev <74693315+xfwdrev@users.noreply.github.com>

--- kernel_platform/build/build.sh
+++ kernel_platform/build/build.sh
@@ -825,7 +825,7 @@ fi
     # Custom Defconfig
     echo "========================================================"
     echo " Merging custom defconfig with .config"
-    (cd ${OUT_DIR} && ${MERGE_CONFIG} -m .config ${ANDROID_BUILD_TOP}/custom_defconfigs/b4q_defconfig)
+    (cd ${OUT_DIR} && ${MERGE_CONFIG} -m .config ${ANDROID_BUILD_TOP}/${CUST_DEFCONFIG})
     (cd ${OUT_DIR} && make O=${OUT_DIR} ${TOOL_ARGS} olddefconfig)
 
 if [ "${LTO}" = "none" -o "${LTO}" = "thin" -o "${LTO}" = "full" ]; then
